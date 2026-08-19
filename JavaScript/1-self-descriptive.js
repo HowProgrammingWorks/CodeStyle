97dc8f201ae0deab492c10f7b431d27dfbd68120
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2026-08-19 23:44:32 +0300

    Fix code style

--- JavaScript/1-self-descriptive.js
+++ JavaScript/1-self-descriptive.js
@@ -4,14 +4,14 @@ const SIZE_UNITS = ['', ' Kb', ' Mb', ' Gb', ' Tb', ' Pb', ' Eb', ' Zb', ' Yb'];
 
 const bytesToSize1 = (bytes) => {
   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1000)), 10);
-  return Math.round(bytes / Math.pow(1000, i), 2) + SIZE_UNITS[i];
+  return Math.round(bytes / 1000 ** i) + SIZE_UNITS[i];
 };
 
 const bytesToSize2 = (bytes) => {
   if (bytes === 0) return '0';
   const exp = Math.floor(Math.log(bytes) / Math.log(1000));
-  const size = bytes / Math.pow(1000, exp);
-  const short = Math.round(size, 2);
+  const size = bytes / 1000 ** exp;
+  const short = Math.round(size);
   const unit = SIZE_UNITS[exp];
   return short + unit;
 };
